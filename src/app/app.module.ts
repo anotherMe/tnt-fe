8bba61d415ec8a5ec12bf3060a263dbddd07c8a8
marco <tuplanulla@gmail.com>
2021-06-05 21:48:38 +0200

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasklistComponent } from './tasklist/tasklist.component';
 import { TaskComponent } from './tasklist/task/task.component';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  { path: 'task', component: TasklistComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: '', redirectTo: '/task', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     TasklistComponent,
-    TaskComponent
+    TaskComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
